a27006d8689939113848a299de3f6366b4326a73
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2025-02-17 19:05:29 +0530

    Update config1.py

--- config1.py
+++ config1.py
@@ -4,7 +4,7 @@ API_SECRET = "RhAuSUL6iipT1o1yA9yDTdZoRTdqcLM2KSPYxtvrLMBmGlY8KeljnaNoPfFfhltf"
 API_PASSPHRASE = "9025014840"
 
 # 3Commas webhook details
-THREE_COMMAS_WEBHOOK_URL = "https://api.3commas.io/signal_bots/webhooks"
+THREECOMMAS_WEBHOOK_URL = "https://api.3commas.io/signal_bots/webhooks"
 SECRET_1 = "eyJhbGciOiJIUzI1NiJ9.eyJzaWduYWxzX3NvdXJjZV9pZCI6MTAyNTR9.UpdSFm3FpcmR7hcVaBUOzPp5Q6-1gBRlQA21sH0ieHo"
 SECRET_2 = "eyJhbGciOiJIUzI1NiJ9.eyJzaWduYWxzX3NvdXJjZV9pZCI6ODI4MzV9.ccZIwRRxe781qP0Mtlx62NIkmKyUGs155h433uxE-M0"
 # Telegram Bot Token
